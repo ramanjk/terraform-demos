11cb085d3e82120da8ae8d195d30bfa8270d7126
ramanjk <ramanjaneyulu476@gmail.com>
2022-05-17 17:41:31 +0530

    sec groups updated

--- 010-intro-1/deploy-server.tf
+++ 010-intro-1/deploy-server.tf
@@ -30,5 +30,24 @@ resource "aws_security_group" "myvm1sg" {
     protocol  = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+  ingress {
+    from_port = 22
+    to_port   = 22
+    protocol  = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port = -1
+    to_port   = -1
+    protocol  = "icmp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
 }
 
